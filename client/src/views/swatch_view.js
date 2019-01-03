b4c6de089751cf2e056d06d688a6c716e4f59191
Steven Kay <stevendkay@gmail.com>
2019-01-03 19:17:15 +0000

    now showing swatches in a grid

--- client/src/views/swatch_view.js
+++ client/src/views/swatch_view.js
@@ -5,9 +5,18 @@ const SwatchView = function(colorEntry) {
 SwatchView.prototype.render = function () {
   // returns an element (div) to render this particular colour.
   const divSwatch = document.createElement("div");
-  divSwatch.innerHTML = this.color.name;
-  divSwatch.className = "swatch";
-  divSwatch.style.backgroundColor = this.color.hexcode;
+  const divColorBit = document.createElement("div");
+  const divNameBit = document.createElement("div");
+  divSwatch.className = "swatch-card";
+  // color swatch
+  divColorBit.style.backgroundColor = this.color.hexcode;
+  divColorBit.className = "swatch-card--swatch";
+  // title
+  divNameBit.innerHTML = this.color.name;
+  divNameBit.className = "swatch-card--name";
+  // add kids to parent
+  divSwatch.appendChild(divColorBit);
+  divSwatch.appendChild(divNameBit);
   return divSwatch;
 };
 
